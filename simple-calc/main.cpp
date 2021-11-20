bfe4bfe3d6d1db3f0eba6d2a3b2163ed6a0daa6d
Rob <robzlegz@gmail.com>
2021-11-20 23:58:27 +0200

    simple calc

--- simple-calc/main.cpp
+++ simple-calc/main.cpp
@@ -2,16 +2,30 @@
 #include <string>
 using namespace std;
 
+int get_num(){
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+    cin.clear();
+    cin.ignore(1000, '\n');
+
+    return num;
+}
+
+int get_sum(int& num1, int& num2){
+    int sum = num1 + num2;
+    
+    return sum;
+}
+
 int main(){
     int num1, num2;
 
-    cout << "Enter a number: ";
-    cin >> num1;
+    num1 = get_num();
+    num2 = get_num();
 
-    cout << "Enter second number: ";
-    cin >> num2;
-
-    int sum = num1 + num2;
+    int sum = get_sum(num1, num2);
 
     cout << "Result: " << sum;
 }
