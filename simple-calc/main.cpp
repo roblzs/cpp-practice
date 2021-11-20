c6997c1697c439bb5348414b3f3412ec50fd5dbf
Rob <robzlegz@gmail.com>
2021-11-20 23:52:43 +0200

    simple calc

---
+++ simple-calc/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout << "Enter a number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+
+    cout << "Result: " << sum;
+}
